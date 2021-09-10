e0bac17a7f34d6490defc15ec3611cb4511fc754
Enzo Pasqualini <enzo.pasqualini@hotmail.com>
2021-09-10 14:56:56 -0300

    Pagination

---
+++ backend/src/main/java/com/DevSuperior/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.DevSuperior.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.DevSuperior.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
